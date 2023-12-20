87701635dbea66afde099cd41758c2d3ccb98f22
GokulGuvi123 <154435622+GokulGuvi123@users.noreply.github.com>
2023-12-20 18:28:56 +0530

    Create Print_10_to_50.java

---
+++ Print_10_to_50.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       int num = 10;
+       while(num<51)
+       {
+           System.out.println(num++);
+       }
+    }
+}
